fa66837ccdc77cfbb6f35f9ac6b5d3c7f68b9ef6
restena.winter <restena.winter@2d91f571-0823-40d5-aea7-c74eced9d4e5>
2014-04-30 06:59:37 +0000

    conversion script for new database layout

---
+++ source/trunk/schema/1_0-1_1.sql
@@ -0,0 +1,14 @@
+INSERT INTO `profile_option_dict` VALUES 
+('media:SSID','additional SSID to configure, WPA2/AES only','string',NULL),
+('media:SSID_with_legacy','additional SSID to configure, WPA2/AES and WPA/TKIP','string',NULL),
+('media:wired','should wired interfaces be configured','boolean',NULL),
+('media:remove_SSID','SSIDs to remove during installation','string',NULL),
+('media:consortium_OI','Hotspot 2.0 consortium OIs to configure','string',NULL);
+
+UPDATE institution_option SET option_name = "media:SSID" WHERE option_name = "general:SSID";
+UPDATE institution_option SET option_name = "media:SSID_with_legacy" WHERE option_name = "general:SSID_with_legacy";
+
+DELETE FROM `profile_option_dict` WHERE
+name = "general:SSID" OR
+name = "general:SSID_with_legacy";
+
