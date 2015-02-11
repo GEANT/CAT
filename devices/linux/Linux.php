9832efc4d23969c7a12c4a6bf3fbc9976b7fa32f
Tomasz Wolniewicz <twoln@umk.pl>
2015-02-11 14:22:32 +0000

    File broken - wrong character

--- devices/linux/Linux.php
+++ devices/linux/Linux.php
@@ -1,4 +1,4 @@
-u?php
+<?php
 /* *********************************************************************************
  * (c) 2011-13 DANTE Ltd. on behalf of the GN3 and GN3plus consortia
  * License: see the LICENSE file in the root directory
