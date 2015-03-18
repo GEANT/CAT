dedc14beffd609f250a90eeef2145306304d8ae8
Stefan Winter <stefan.winter@restena.lu>
2015-03-18 07:21:51 +0000

    fix rather obvious source code error

--- core/DBConnection.php
+++ core/DBConnection.php
@@ -138,7 +138,7 @@ class DBConnection {
     public static function isDataRestricted($table, $row) {
             if ($table != "institution_option" && $table != "profile_option")
             return array(); // better safe than sorry: that's an error, so assume nobody is authorised to act on that data
-            select ($table) {
+            switch ($table) {
             case "profile_option":            
                 $blob_query = DBConnection::exec("INST", "SELECT profile_id from $table WHERE row = $row");
                 while ($a = mysqli_fetch_object($blob_query)) // only one row
