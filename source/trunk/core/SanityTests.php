4839ff847bca6580f3c52364ad1da8274b109b5f
restena.winter <restena.winter@2d91f571-0823-40d5-aea7-c74eced9d4e5>
2016-02-18 12:14:06 +0000

    we have 28 distinct options in the db

--- source/trunk/core/SanityTests.php
+++ source/trunk/core/SanityTests.php
@@ -55,7 +55,7 @@ class SanityTest extends CAT {
          ];
 
 /* set $profile_option_ct to the number of rows returned by "SELECT * FROM profile_option_dict" */
-    private $profile_option_ct = 29;
+    private $profile_option_ct = 28;
 /* set $view_admin_ct to the number of rows returned by "desc view_admin" */
     private $view_admin_ct = 8;
 
