b1c8e7e3c0608529fbf38b8773771abcfea8fcd2
Stefan Winter <stefan.winter@restena.lu>
2016-08-26 15:35:16 +0200

    restructure to reduce complexity and improve readability

--- web/resources/inc/header.php
+++ web/resources/inc/header.php
@@ -43,8 +43,9 @@ function defaultPagePrelude($pagetitle, $auth_required = TRUE) {
           <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>";
 
     $cssUrl = valid_host($_SERVER['HTTP_HOST']);
-    if ($cssUrl === FALSE)
-        exit(1);
+    if ($cssUrl === FALSE) {
+        throw new Exception("We don't know our own hostname?!");
+    }
     // we need to construct the right path to the consortium logo; we are either
     // in the admin area or on the main index.php ...
     if (strpos($_SERVER['PHP_SELF'], "admin/") !== FALSE) {
@@ -55,20 +56,69 @@ function defaultPagePrelude($pagetitle, $auth_required = TRUE) {
         $cssUrl .= substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/")) . "/resources/css/cat.css.php";
     }
 
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on")
-        $cssUrl = "https://" . $cssUrl;
-    else
-        $cssUrl = "http://" . $cssUrl;
+    $cssUrl = "//" . $cssUrl; // omitting http or https means "on same protocol
+    
     echo "<link rel='stylesheet' type='text/css' href='$cssUrl' />";
     echo "<title>" . htmlspecialchars($pagetitle) . "</title>";
 
     return $cat;
 }
 
-function headerDiv($area) {
+function headerDiv($cap1) {
+    $place = parse_url($_SERVER['REQUEST_URI']);
     ?>
     <div class='header'>
-        <?php
+        <div id='header_toprow'>
+            <div id='header_captions' style='display:inline-block; float:left; min-width:400px;'>
+                <h1><?php echo $cap1; ?></h1>
+            </div><!--header_captions-->
+            <div id='langselection' style='padding-top:20px; padding-left:10px;'>
+                <form action='<?php echo $place['path']; ?>' method='GET' accept-charset='UTF-8'><?php echo _("View this page in"); ?>&nbsp;
+                    <select id='lang' name='lang' onchange='this.form.submit()'>
+                        <?php
+                        foreach (Config::$LANGUAGES as $lang => $value) {
+                            echo "<option value='$lang' " . (strtoupper($language) == strtoupper($lang) ? "selected" : "" ) . " >" . $value['display'] . "</option> ";
+                        }
+                        ?>
+                    </select>
+                    <?php
+                    foreach ($_GET as $var => $value) {
+                        if ($var != "lang" && $value != "") {
+                            echo "<input type='hidden' name='" . htmlspecialchars($var) . "' value='" . htmlspecialchars($value) . "'>";
+                        }
+                    }
+                    ?>
+                </form>
+            </div><!--langselection-->
+            <?php
+            $logoUrl = valid_host($_SERVER['HTTP_HOST']);
+            if ($logoUrl === FALSE) {
+                throw new Exception("We don't know our own hostname?!");
+            }
+            // we need to construct the right path to the consortium logo; we are either
+            // in the admin area or on the main index.php ...
+            if (strpos($_SERVER['PHP_SELF'], "admin/") === FALSE) {
+                $logoUrl .= substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/")) . "/resources/images/consortium_logo.png";
+            }
+            else {
+                $logoUrl .= substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/admin/")) . "/resources/images/consortium_logo.png";
+            }
+            $logoUrl = "//" . $logoUrl;
+            ?>
+            <div class='consortium_logo'>
+                <img id='test_locate' src='$logoUrl' alt='Consortium Logo'>
+            </div> <!-- consortium_logo -->
+
+        </div><!--header_toprow-->
+    </div> <!-- header -->
+    <?php
+}
+
+function productheader($area, $language) {
+    // this <div is closing in footer, keep it in PHP for Netbeans syntax
+    // highlighting to work
+    echo "<div class='maincontent'>";
+
         switch ($area) {
             case "ADMIN-IDP":
                 $cap1 = Config::$APPEARANCE['productname_long'];
@@ -104,58 +154,9 @@ function headerDiv($area) {
                 $cap1 = Config::$APPEARANCE['productname_long'];
                 $cap2 = "It is an error if you ever see this string.";
         }
-        ?>
-        <div id='header_toprow'>
-            <div id='header_captions' style='display:inline-block; float:left; min-width:400px;'>
-                <h1><?php echo $cap1; ?></h1>
-            </div><!--header_captions-->
-            <div id='langselection' style='padding-top:20px; padding-left:10px;'>
-                <form action='<?php echo $place['path']; ?>' method='GET' accept-charset='UTF-8'><?php echo _("View this page in"); ?>&nbsp;
-                    <select id='lang' name='lang' onchange='this.form.submit()'>
-                        <?php
-                        foreach (Config::$LANGUAGES as $lang => $value) {
-                            echo "<option value='$lang' " . (strtoupper($language) == strtoupper($lang) ? "selected" : "" ) . " >" . $value['display'] . "</option> ";
-                        }
-                        ?>
-                    </select>
-                    <?php
-                    foreach ($_GET as $var => $value) {
-                        if ($var != "lang" && $value != "")
-                            echo "<input type='hidden' name='" . htmlspecialchars($var) . "' value='" . htmlspecialchars($value) . "'>";
-                    }
-                    ?>
-                </form>
-            </div><!--langselection-->
-            <?php
-            $logoUrl = valid_host($_SERVER['HTTP_HOST']);
-            if ($logoUrl === FALSE)
-                exit(1);
-            // we need to construct the right path to the consortium logo; we are either
-            // in the admin area or on the main index.php ...
-            if (strpos($_SERVER['PHP_SELF'], "admin/") === FALSE)
-                $logoUrl .= substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/")) . "/resources/images/consortium_logo.png";
-            else
-                $logoUrl .= substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/admin/")) . "/resources/images/consortium_logo.png";
-            if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on")
-                $logoUrl = "https://" . $logoUrl;
-            else
-                $logoUrl = "http://" . $logoUrl;
-            ?>
-            <div class='consortium_logo'>
-                <img id='test_locate' src='$logoUrl' alt='Consortium Logo'>
-            </div> <!-- consortium_logo -->
 
-        </div><!--header_toprow-->
-    </div> <!-- header -->
-    <?php
-}
 
-function productheader($area, $language) {
-    $place = parse_url($_SERVER['REQUEST_URI']);
-    // this <div is closing in footer, keep it in PHP for Netbeans syntax
-    // highlighting to work
-    echo "<div class='maincontent'>";
-    echo headerDiv($area);
+    echo headerDiv($cap1);
     // content from here on will SCROLL instead of being fixed at the top
     echo "<div class='pagecontent'>"; // closes in footer again
     echo "<div class='trick'>"; // closes in footer again
@@ -165,10 +166,11 @@ function productheader($area, $language) {
             <h2><?php echo $cap2; ?></h2>
         </div><!--secondarycaptions-->";
         <?php
-        if (isset(Config::$APPEARANCE['MOTD']) && Config::$APPEARANCE['MOTD'] != "")
+        if (isset(Config::$APPEARANCE['MOTD']) && Config::$APPEARANCE['MOTD'] != "") {
             echo "<div id='header_MOTD' style='display:inline-block; padding-left:20px;vertical-align:top;'>
               <p class='MOTD'>" . Config::$APPEARANCE['MOTD'] . "</p>
               </div><!--header_MOTD-->";
+        }
         ?>
         <div class='sidebar'><p>
                 <?php
