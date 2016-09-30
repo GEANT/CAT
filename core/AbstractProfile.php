4d885ad8dc5fdf6ddec1ee8c63e5caa46ecc2026
Stefan Winter <stefan.winter@restena.lu>
2016-09-30 10:50:55 +0200

    additional input check before the first SQL query with input data

--- core/AbstractProfile.php
+++ core/AbstractProfile.php
@@ -113,6 +113,10 @@ abstract class AbstractProfile extends EntityWithDBProperties {
     public function __construct($profileId, $idpObject = NULL) {
         $this->databaseType = "INST";
         parent::__construct(); // we now have access to our database handle and logging
+        // first make sure that we are operating on numeric identifiers
+        if (!is_numeric($profileId)) {
+            throw new Exception("Non-numeric Profile identifier was passed to AbstarctProfile constructor!");
+        }
         $this->loggerInstance->debug(3, "--- BEGIN Constructing new AbstractProfile object ... ---\n");
         $profile = $this->databaseHandle->exec("SELECT inst_id FROM profile WHERE profile_id = $profileId");
         if (!$profile || $profile->num_rows == 0) {
