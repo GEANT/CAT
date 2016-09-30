0f2b34efeb7905e47877fb2424398b8168cfdabd
Stefan Winter <stefan.winter@restena.lu>
2016-09-30 11:31:25 +0200

    properly check for valid IPv4 and IPv6 addresses

--- web/admin/radius_tests.php
+++ web/admin/radius_tests.php
@@ -123,8 +123,9 @@ if (isset($_REQUEST['profile_id'])) {
 }
 
 $host = $_REQUEST['src'];
-if (!preg_match('/^[0-9\.:]*$/', $host)) {
-    exit;
+// check if this is a valid IP address
+if (!filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+    throw new Exception("The input is not a valid IP address from acceptable IP ranges!");
 }
 
 $hostindex = $_REQUEST['hostindex'];
