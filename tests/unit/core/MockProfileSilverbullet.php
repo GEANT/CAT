24c7d24edc804e72631856df5b77b088d873b632
zhilwis <zilvinas@vaira.net>
2017-01-04 15:42:27 +0200

    Mock class updated

--- tests/unit/core/MockProfileSilverbullet.php
+++ tests/unit/core/MockProfileSilverbullet.php
@@ -35,6 +35,7 @@ class MockProfileSilverbullet extends ProfileSilverbullet{
         if($this->databaseHandle->exec("INSERT INTO profile (inst_id, realm) VALUES($this->instId, 'test.realm.tst')")){
             $this->identifier = $this->databaseHandle->lastID();
         }
+        $this->attributes = array(array('name' => 'hiddenprofile:tou_accepted'));
     }
     
     public function delete(){
