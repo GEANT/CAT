f316df888f172788b74cf8ce7347c680c8c220ac
Tomasz Wolniewicz <twoln@umk.pl>
2017-03-01 12:07:11 +0100

    Adding a global class to the top index.php

--- web/index.php
+++ web/index.php
@@ -18,15 +18,25 @@
 require_once(dirname(dirname(__FILE__)) . "/config/_config.php");
 require_once("admin/inc/input_validation.inc.php");
 
-if (!empty($_REQUEST['idp'])) { // determine skin to use based on NROs preference
-    $idp = valid_IdP($_REQUEST['idp']);
-    $fed = valid_Fed($idp->federation);
-    $fedskin = $fed->getAttributes("fed:desired_skin");
+class Gui extends \core\UserAPI {
+    public function __construct() {
+        parent::__construct();
+        if (!empty($_REQUEST['idp'])) { // determine skin to use based on NROs preference
+            $idp = valid_IdP($_REQUEST['idp']);
+            $fed = valid_Fed($idp->federation);
+            $fedskin = $fed->getAttributes("fed:desired_skin");
+        }
+        $this->skinObject = new \core\Skinjob( $_REQUEST['skin'] ?? $_SESSION['skin'] ?? $fedskin[0] ?? CONFIG['APPEARANCE']['skins'][0]);
+        $this->langObject = new \core\Language();
+    }
+    public $loggerInstance;
+    public $skinObject;
+    public $langObject;
 }
-// ... unless overwritten by direct GET/POST parameter in the request
+
+$Gui = new Gui();
+// ... unless overwritten by direct GET/POST parameter in the request or a SESSION setting
 // ... with last resort being the default skin (first one in the configured skin list is the default)
 
-$skinObject = new \core\Skinjob( $_REQUEST['skin'] ?? $fedskin[0] ?? CONFIG['APPEARANCE']['skins'][0]);
-
 // and now, serve actual data
-include("skins/".$skinObject->skin."/index.php");
+include("skins/".$Gui->skinObject->skin."/index.php");
