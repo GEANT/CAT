82359f7e6f82232f28434d8ec608b6ea70d521c1
Stefan Winter <stefan.winter@restena.lu>
2017-08-17 15:27:09 +0200

    whitespace change (include demo code for config comparison: which values from the template config are MISSING or still at DEFAULT? May be useful as a check on the superadmin page in the future...

--- config/_config.php
+++ config/_config.php
@@ -10,7 +10,53 @@
 
 require_once ("autoloader.php");
 require_once(__DIR__."/../packageRoot.php");
+
+/* This code block compares the template config against the actual one to find
+ * out which of the values are MISSING, which are still at DEFAULT and which
+ * have been CHANGED.
+
+function recursiveConfCheck($template, $real) {
+    
+    $result = [];
+    
+    foreach ($template as $key => $value) {
+        if (!isset($real[$key])) {
+            $result[$key] = "MISSING";
+        } elseif (is_array($value)) {
+            $result[$key] = recursiveConfCheck($value, $real[$key]);
+        } elseif ($value === $real[$key]) {
+            $result[$key] = "DEFAULT";            
+        } else {
+            $result[$key] = "CHANGED";
+        }
+    }
+    return $result;
+}
+
+// first, fetch and store the /template/ config so that we can find missing
+// bits in the actual config. Since this is a const, we need to first load
+// the template, alter the const name, save it, and include it
+
+$templateConfig = file_get_contents(ROOT."/config/config-master-template.php");
+$newTemplateConfig = preg_replace("/const CONFIG/", "const TEMPLATE_CONFIG", $templateConfig);
+file_put_contents(ROOT."/var/tmp/temp-master.php", $newTemplateConfig);
+include(ROOT."/var/tmp/temp-master.php");
+unlink(ROOT."/var/tmp/temp-master.php");
+
+// this is the actual config
+
 include(ROOT."/config/config-master.php");
+
+// as a test, run this, display in browser and exit
+
+echo "<pre>";
+print_r(TEMPLATE_CONFIG);
+print_r(CONFIG);
+print_r(recursiveConfCheck(TEMPLATE_CONFIG, CONFIG));
+echo "</pre>";
+exit;
+
+*/
 
 /* load sub-configs if we are dealing with those in this installation */
 
