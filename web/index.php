6a20237eada89b9ab3ad6b6681033efed58a86ec
Tomasz Wolniewicz <twoln@umk.pl>
2017-08-24 13:47:14 +0200

    preparations for skin modification

--- web/index.php
+++ web/index.php
@@ -20,6 +20,10 @@ require_once(dirname(dirname(__FILE__)) . "/config/_config.php");
 $Gui = new \web\lib\user\Gui();
 // ... unless overwritten by direct GET/POST parameter in the request or a SESSION setting
 // ... with last resort being the default skin (first one in the configured skin list is the default)
+error_reporting(E_ALL | E_STRICT);
+$Gui->langObject->setTextDomain("web_user");
+$Gui->loggerInstance->debug(4, "\n---------------------- index.php START --------------------------\n");
+$Gui->defaultPagePrelude();
 
 // and now, serve actual data
 include("skins/".$Gui->skinObject->skin."/index.php");
