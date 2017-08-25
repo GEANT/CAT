ccfc9053e5ecce8be873a8f7ba270b721c30e837
Stefan Winter <stefan.winter@restena.lu>
2017-08-25 08:07:47 +0200

    parse CRL URLs a bit better than before

--- core/diag/RADIUSTests.php
+++ core/diag/RADIUSTests.php
@@ -284,10 +284,10 @@ class RADIUSTests extends AbstractTest {
         $returnresult = 0;
         if (!isset($cert['full_details']['extensions']['crlDistributionPoints'])) {
             $returnresult = RADIUSTests::CERTPROB_NO_CDP;
-        } else if (!preg_match("/^.*URI\:(http)(.*)$/", str_replace(["\r", "\n"], '', $cert['full_details']['extensions']['crlDistributionPoints']), $crlUrl)) {
+        } else if (!preg_match("/^.*URI\:(http)(.*)$/", str_replace(["\r", "\n"], ' ', $cert['full_details']['extensions']['crlDistributionPoints']), $crlUrl)) {
             $returnresult = RADIUSTests::CERTPROB_NO_CDP_HTTP;
         } else { // first and second sub-match is the full URL... check it
-            $crlcontent = \core\common\OutsideComm::downloadFile($crlUrl[1] . $crlUrl[2]);
+            $crlcontent = \core\common\OutsideComm::downloadFile(trim($crlUrl[1] . $crlUrl[2]));
             if ($crlcontent === FALSE) {
                 $returnresult = RADIUSTests::CERTPROB_NO_CRL_AT_CDP_URL;
             }
