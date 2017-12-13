4f221cdc3911a7bb8418243f2d9f63d1ae3f089e
Stefan Winter <stefan.winter@restena.lu>
2017-12-13 15:25:46 +0100

    wrap logout check in try/catch to not get excited about NOSTATE Exceptions

--- web/admin/logout_check.php
+++ web/admin/logout_check.php
@@ -1,11 +1,12 @@
 <?php
-/* 
- *******************************************************************************
+
+/*
+ * ******************************************************************************
  * Copyright 2011-2017 DANTE Ltd. and GÉANT on behalf of the GN3, GN3+, GN4-1 
  * and GN4-2 consortia
  *
  * License: see the web/copyright.php file in the file structure
- *******************************************************************************
+ * ******************************************************************************
  */
 ?>
 <?php
@@ -13,12 +14,15 @@
 require_once(dirname(dirname(dirname(__FILE__))) . "/config/_config.php");
 require_once(CONFIG['AUTHENTICATION']['ssp-path-to-autoloader']);
 
-use SimpleSAML_Auth_State;
-
 $deco = new \web\lib\admin\PageDecoration();
 
-$state = SimpleSAML_Auth_State::loadState((string) $_REQUEST['LogoutState'], 'MyLogoutState');
-$ls = $state['saml:sp:LogoutStatus']; /* Only works for SAML SP */
+try {
+    $state = SimpleSAML_Auth_State::loadState((string) $_REQUEST['LogoutState'], 'MyLogoutState');
+    $ls = $state['saml:sp:LogoutStatus']; /* Only works for SAML SP */
+} catch (Exception $except) {
+    $ls = ['Code' => 'NOSTATE'];
+}
+
 if ($ls['Code'] === 'urn:oasis:names:tc:SAML:2.0:status:Success' && !isset($ls['SubCode'])) {
     /* Successful logout. */
     $url = "//" . htmlspecialchars($_SERVER['SERVER_NAME']) . substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], "/admin/logout_check.php"));
