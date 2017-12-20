53a0918f36c43c6dd6769eb7e9d7fa9d12b1b235
Stefan Winter <stefan.winter@restena.lu>
2017-12-20 12:44:19 +0100

    de-duplicate (SB downloads are never in the cache, so it doesn't hurt to do the cache check just like with normal downloads - and then the code becomes identical

--- utils/cleanup.php
+++ utils/cleanup.php
@@ -1,11 +1,12 @@
 <?php
-/* 
- *******************************************************************************
+
+/*
+ * ******************************************************************************
  * Copyright 2011-2017 DANTE Ltd. and GÉANT on behalf of the GN3, GN3+, GN4-1 
  * and GN4-2 consortia
  *
  * License: see the web/copyright.php file in the file structure
- *******************************************************************************
+ * ******************************************************************************
  */
 
 // called by cleanup.sh (which in turn should run via cron every hour)
@@ -13,10 +14,9 @@
 /**
  * This script deletes obsolete directories from installer cache and siverbullet directory
  */
-
 require_once(dirname(dirname(__FILE__)) . "/config/_config.php");
 
-$downloads = dirname(dirname(__FILE__)) . "/var/installer_cache";
+
 $tm = time();
 
 $Cache = [];
@@ -26,37 +26,27 @@ while ($r = mysqli_fetch_row($result)) {
     $e = explode('/', $r[0]);
     $Cache[$e[count($e) - 2]] = 1;
 }
-
-if ($handle = opendir($downloads)) {
-    /* This is the correct way to loop over the directory. */
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry === '.' || $entry === '..' || $entry === '.gitignore') {
-            continue;
+$downloadsDirs = [
+    'site_installers' => dirname(dirname(__FILE__)) . "/var/installer_cache",
+    'silverbullet' => dirname(dirname(__FILE__)) . "/var/silverbullet"
+];
+foreach ($downloadsDirs as $downloads) {
+    if ($handle = opendir($downloads)) {
+        /* This is the correct way to loop over the directory. */
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry === '.' || $entry === '..' || $entry === '.gitignore') {
+                continue;
+            }
+            $ftime = $tm - filemtime($downloads . '/' . $entry);
+            if ($ftime < 3600) {
+                continue;
+            }
+            if (isset($Cache[$entry])) {
+                continue;
+            }
+            \core\common\Entity::rrmdir($downloads . '/' . $entry);
+            print "$entry\n";
         }
-        $ftime = $tm - filemtime($downloads . '/' . $entry);
-        if ($ftime < 3600) {
-            continue;
-        }
-        if (isset($Cache[$entry])) {
-            continue;
-        }
-       \core\common\Entity::rrmdir($downloads . '/' . $entry);
-        print "$entry\n";
+        closedir($handle);
     }
-    closedir($handle);
-}
-$downloads = dirname(dirname(__FILE__)) . "/var/silverbullet";
-if ($handle = opendir($downloads)) {
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry === '.' || $entry === '..' || $entry === '.gitignore') {
-            continue;
-        }
-        $ftime = $tm - filemtime($downloads . '/' . $entry);
-        if ($ftime < 3600) {
-            continue;
-        }
-       \core\common\Entity::rrmdir($downloads . '/' . $entry);
-        print "$entry\n";
-    }
-    closedir($handle);
 }
