0970e45bedfa97bfd4b440e641b4e2be46f1a5b2
Tomasz Wolniewicz <twoln@umk.pl>
2018-01-11 15:51:10 +0100

    removing the classic skin

--- config/config-master-template.php
+++ config/config-master-template.php
@@ -59,7 +59,7 @@ const CONFIG = [
         // in "About CAT" then)
         'webcert_CRLDP' => ['list', 'of', 'CRL', 'pointers'],
         'webcert_OCSP' => ['list', 'of', 'OCSP', 'pointers'],
-        'skins' => ["modern", "classic", "example"],
+        'skins' => ["modern", "example"],
         // get your key here: https://developers.google.com/maps/documentation/javascript/get-api-key?refresh=1
         'google_maps_api_key' => '',
     ],
