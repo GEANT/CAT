861756e3e33dfa62759f631a00a83814b81924f5
Tomasz Wolniewicz <twoln@umk.pl>
2018-02-07 15:01:07 +0100

    updating silverbullet to use module-specific css

--- web/skins/modern/accountstatus/accountstatus.php
+++ web/skins/modern/accountstatus/accountstatus.php
@@ -29,6 +29,7 @@ include(dirname(__DIR__) . '/user/js/cat_js.php');
 </script>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 <link rel="stylesheet" media="screen" type="text/css" href="<?php echo $Gui->skinObject->findResourceUrl("CSS", "cat-user.css"); ?>" />
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo $Gui->skinObject->findResourceUrl("CSS", "accountstatus.css", "accountstatus"); ?>" />
 
 </head>
 <body>
