225775cd4f47d3be60facd4f745045edbc30a481
Stefan Winter <stefan.winter@restena.lu>
2018-02-13 15:12:11 +0100

    eliinate more parameters which don't have to exist

--- devices/apple_mobileconfig/mobileconfigSuperclass.php
+++ devices/apple_mobileconfig/mobileconfigSuperclass.php
@@ -41,6 +41,8 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
     private $lang;
     static private $iPhonePayloadPrefix = "org.1x-config";
 
+    private $clientCertUUID;
+    
     public function __construct() {
         parent::__construct();
         // that's what all variants support. Sub-classes can change it.
@@ -155,16 +157,16 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         // if we are in silverbullet, we will need a whole own block for the client credential
         // and also for the profile expiry
 
-        $clientCertUUID = NULL;
+        $this->clientCertUUID = NULL;
         if ($eapType['INNER'] == \core\common\EAP::NE_SILVERBULLET) {
             $blockinfo = $this->clientP12Block();
             $outputXml .= $blockinfo['block'];
-            $clientCertUUID = $blockinfo['UUID'];
+            $this->clientCertUUID = $blockinfo['UUID'];
         }
 
         $outputXml .= $this->allCA();
 
-        $outputXml .= $this->allNetworkBlocks($clientCertUUID);
+        $outputXml .= $this->allNetworkBlocks();
 
         $outputXml .= "</array>";
         $outputXml .= $this->generalPayload();
@@ -216,9 +218,9 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         return $out;
     }
 
-    private function listCAUuids($caArray) {
+    private function listCAUuids() {
         $retval = [];
-        foreach ($caArray as $ca) {
+        foreach ($this->attributes['internal:CAs'][0] as $ca) {
             $retval[] = $ca['uuid'];
         }
         return $retval;
@@ -264,6 +266,8 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
     }
 
     private $serial;
+    private $removeSerial;
+    private $caSerial;
 
     private function eapBlock($eapType) {
         $realm = $this->determineOuterIdString();
@@ -289,7 +293,7 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         }
         $retval .= "<key>PayloadCertificateAnchorUUID</key>
                          <array>";
-        foreach ($this->listCAUuids($this->attributes['internal:CAs'][0]) as $uuid) {
+        foreach ($this->listCAUuids() as $uuid) {
             $retval .= "
 <string>$uuid</string>";
         }
@@ -337,7 +341,7 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         return $buffer;
     }
 
-    private function networkBlock($ssid, $consortiumOi, $wired, $clientCertUUID) {
+    private function networkBlock($ssid, $consortiumOi, $wired) {
         $escapedSSID = htmlspecialchars($ssid, ENT_XML1, 'UTF-8');
         $eapType = $this->selectedEap;
         $payloadIdentifier = "wifi." . $this->serial;
@@ -388,11 +392,11 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         $retval .= $this->proxySettings();
         $retval .= $setupModesString;
         if ($eapType['INNER'] == \core\common\EAP::NE_SILVERBULLET) {
-            if ($clientCertUUID === NULL) {
+            if ($this->clientCertUUID === NULL) {
                 throw new Exception("Silverbullet REQUIRES a client certificate and we need to know the UUID!");
             }
             $retval .= "<key>PayloadCertificateUUID</key>
-                        <string>$clientCertUUID</string>";
+                        <string>$this->clientCertUUID</string>";
         }
         $retval .= "
                <key>PayloadUUID</key>
@@ -404,7 +408,7 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         return $retval;
     }
 
-    private function removenetworkBlock($ssid, $sequence) {
+    private function removenetworkBlock($ssid) {
         $retval = "
 <dict>
 	<key>AutoJoin</key>
@@ -420,7 +424,7 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
 	<key>PayloadDisplayName</key>
 	<string>" . _("Disabled WiFi network") . "</string>
 	<key>PayloadIdentifier</key>
-	<string>" . self::$iPhonePayloadPrefix . ".$this->massagedConsortium.$this->massagedCountry.$this->massagedInst.$this->massagedProfile.$this->lang.wifi.disabled.$sequence</string>
+	<string>" . self::$iPhonePayloadPrefix . ".$this->massagedConsortium.$this->massagedCountry.$this->massagedInst.$this->massagedProfile.$this->lang.wifi.disabled.$this->removeSerial</string>
 	<key>PayloadType</key>
 	<string>com.apple.wifi.managed</string>
 	<key>PayloadUUID</key>
@@ -435,22 +439,24 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
         return $retval;
     }
 
-    private function allNetworkBlocks($clientcertUUID) {
+    private function allNetworkBlocks() {
         $retval = "";
         $this->serial = 0;
 
         foreach (array_keys($this->attributes['internal:SSID']) as $ssid) {
-            $retval .= $this->networkBlock($ssid, NULL, FALSE, $clientcertUUID);
+            $retval .= $this->networkBlock($ssid, NULL, FALSE);
         }
         if (isset($this->attributes['media:wired']) && get_class($this) == "Device_mobileconfig_os_x") {
-            $retval .= $this->networkBlock("IRRELEVANT", NULL, TRUE, $clientcertUUID);
+            $retval .= $this->networkBlock("IRRELEVANT", NULL, TRUE);
         }
         if (count($this->attributes['internal:consortia']) > 0) {
-            $retval .= $this->networkBlock("IRRELEVANT", $this->attributes['internal:consortia'], FALSE, $clientcertUUID);
+            $retval .= $this->networkBlock("IRRELEVANT", $this->attributes['internal:consortia'], FALSE);
         }
         if (isset($this->attributes['media:remove_SSID'])) {
-            foreach ($this->attributes['media:remove_SSID'] as $index => $removeSSID) {
-                $retval .= $this->removenetworkBlock($removeSSID, $index);
+            $this->removeSerial = 0;
+            foreach ($this->attributes['media:remove_SSID'] as $removeSSID) {
+                $retval .= $this->removenetworkBlock($removeSSID);
+                $this->removeSerial = $this->removeSerial + 1;
             }
         }
         return $retval;
@@ -458,10 +464,10 @@ abstract class mobileconfigSuperclass extends \core\DeviceConfig {
 
     private function allCA() {
         $retval = "";
-        $iterator = 0;
+        $this->caSerial = 0;
         foreach ($this->attributes['internal:CAs'][0] as $ca) {
-            $retval .= $this->caBlob($ca['uuid'], $ca['pem'], $iterator);
-            $iterator = $iterator + 1;
+            $retval .= $this->caBlob($ca);
+            $this->caSerial = $this->caSerial + 1;
         }
         return $retval;
     }
@@ -509,16 +515,16 @@ $mimeFormatted
         <date>" . $expiryTime->format("Y-m-d") . "T" . $expiryTime->format("H:i:s") . "Z</date>";
     }
 
-    private function caBlob($uuid, $pem, $serial) {
+    private function caBlob($ca) {
         // cut lines with CERTIFICATE
-        $stage1 = preg_replace('/-----BEGIN CERTIFICATE-----/', '', $pem);
+        $stage1 = preg_replace('/-----BEGIN CERTIFICATE-----/', '', $ca['pem']);
         $stage2 = preg_replace('/-----END CERTIFICATE-----/', '', $stage1);
         $trimmedPem = trim($stage2);
 
         $stream = "
             <dict>
                <key>PayloadCertificateFileName</key>
-               <string>$uuid.der</string>
+               <string>".$ca['uuid'].".der</string>
                <key>PayloadContent</key>
                <data>
 " . $trimmedPem . "</data>
@@ -527,12 +533,12 @@ $mimeFormatted
                <key>PayloadDisplayName</key>
                <string>" . _("Identity Provider's CA") . "</string>
                <key>PayloadIdentifier</key>
-               <string>" . self::$iPhonePayloadPrefix . ".$this->massagedConsortium.$this->massagedCountry.$this->massagedInst.$this->massagedProfile.credential.$serial</string>
+               <string>" . self::$iPhonePayloadPrefix . ".$this->massagedConsortium.$this->massagedCountry.$this->massagedInst.$this->massagedProfile.credential.$this->caSerial</string>
                <key>PayloadOrganization</key>
                <string>" . $this->massagedConsortium . ".1x-config.org</string>
                <key>PayloadType</key>
                <string>com.apple.security.root</string>
-               <key>PayloadUUID</key><string>" . $uuid . "</string>
+               <key>PayloadUUID</key><string>" . $ca['uuid'] . "</string>
                <key>PayloadVersion</key>
                <integer>1</integer>
             </dict>";
