a3022846d3dfec3fc12f9b9d02958148058c29c0
Stefan Winter <stefan.winter@restena.lu>
2018-04-06 16:54:43 +0200

    little tool to check ECDSA stuff

---
+++ web/key_ecdsa.php
@@ -0,0 +1,25 @@
+<?php
+$config = array(
+    "digest_alg" => "sha512",
+    "private_key_type" => OPENSSL_KEYTYPE_EC,
+);
+   
+// Create the private and public key
+$res = openssl_pkey_new($config);
+
+// Extract the private key from $res to $privKey
+openssl_pkey_export($res, $privKey);
+
+// Extract the public key from $res to $pubKey
+$pubKey = openssl_pkey_get_details($res);
+$pubKey = $pubKey["key"];
+
+$data = 'plaintext data goes here';
+
+// Encrypt the data to $encrypted using the public key
+openssl_public_encrypt($data, $encrypted, $pubKey);
+
+// Decrypt the data using the private key and store the results in $decrypted
+openssl_private_decrypt($encrypted, $decrypted, $privKey);
+
+echo $decrypted;
