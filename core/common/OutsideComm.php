dfadeef3dbb1ca9952315af3c47f8aca0fa9cdf9
Stefan Winter <stefan.winter@restena.lu>
2018-04-25 14:33:26 +0200

    make invitation send code more readable with less conditions

--- core/common/OutsideComm.php
+++ core/common/OutsideComm.php
@@ -212,10 +212,11 @@ class OutsideComm {
     }
 
     const INVITE_CONTEXTS = [
-        0 => "CO-ADMIN", 
-        1 => "NEW-FED", 
+        0 => "CO-ADMIN",
+        1 => "NEW-FED",
         2 => "EXISTING-FED",
     ];
+
     /**
      * 
      * @param string $targets one or more mail addresses, comma-separated
@@ -231,69 +232,34 @@ class OutsideComm {
         if ($introtext == OutsideComm::INVITE_CONTEXTS[1] && $federation === NULL) { // comes from fed admin, so federation must be set
             throw new \Exception("Invitation from a fed admin, but we do not know the corresponding federation!");
         }
+        $mail = OutsideComm::mailHandle();
         $cat = new \core\CAT();
         // we have a few stock intro texts on file
-        $introTexts = 
-                [
-        OutsideComm::INVITE_CONTEXTS[0] => sprintf(_("a %s of the %s %s \"%s\" has invited you to manage the %s together with him."), $cat->nomenclature_fed, CONFIG_CONFASSISTANT['CONSORTIUM']['display_name'], $cat->nomenclature_inst, $idpPrettyName, $cat->nomenclature_inst),  
-        OutsideComm::INVITE_CONTEXTS[1] => sprintf(_("a %s %s has invited you to manage the future %s  \"%s\" (%s)."), CONFIG_CONFASSISTANT['CONSORTIUM']['display_name'], $cat->nomenclature_fed, $cat->nomenclature_inst, $idpPrettyName, strtoupper($federation->tld)),
-        OutsideComm::INVITE_CONTEXTS[2] => sprintf(_("a %s %s has invited you to manage the %s  \"%s\"."), CONFIG_CONFASSISTANT['CONSORTIUM']['display_name'], $cat->nomenclature_fed, $cat->nomenclature_inst, $idpPrettyName),
-                ];
+        $introTexts = [
+            OutsideComm::INVITE_CONTEXTS[0] => sprintf(_("a %s of the %s %s \"%s\" has invited you to manage the %s together with him."), $cat->nomenclature_fed, CONFIG_CONFASSISTANT['CONSORTIUM']['display_name'], $cat->nomenclature_inst, $idpPrettyName, $cat->nomenclature_inst),
+            OutsideComm::INVITE_CONTEXTS[1] => sprintf(_("a %s %s has invited you to manage the future %s  \"%s\" (%s)."), CONFIG_CONFASSISTANT['CONSORTIUM']['display_name'], $cat->nomenclature_fed, $cat->nomenclature_inst, $idpPrettyName, strtoupper($federation->tld)),
+            OutsideComm::INVITE_CONTEXTS[2] => sprintf(_("a %s %s has invited you to manage the %s  \"%s\"."), CONFIG_CONFASSISTANT['CONSORTIUM']['display_name'], $cat->nomenclature_fed, $cat->nomenclature_inst, $idpPrettyName),
+        ];
         $validity = sprintf(_("This invitation is valid for 24 hours from now, i.e. until %s."), strftime("%x %X", time() + 86400));
         // need some nomenclature
-        
         // are we on https?
         $proto = "http://";
         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") {
             $proto = "https://";
         }
         // then, send out the mail
-        $message = _("Hello,") . "
-    
-" . wordwrap($introTexts[$introtext]." ".$validity, 72) . "
-    
-";
+        $message = _("Hello,") . "\n\n" . wordwrap($introTexts[$introtext] . " " . $validity, 72) . "\n\n";
+        // default means we don't have a Reply-To.
+        $replyToMessage .= wordwrap(_("manually. Please do not reply to this mail; this is a send-only address."));
 
-        if ($federation !== NULL) { // see if we are supposed to add a custom message
+        if ($federation !== NULL) {
+            // see if we are supposed to add a custom message
             $customtext = $federation->getAttributes('fed:custominvite');
             if (count($customtext) > 0) {
-                $message .= wordwrap(sprintf(_("Additional message from your %s administrator:"), $cat->nomenclature_fed), 72) . "
----------------------------------
-"
-                        . wordwrap($customtext[0]['value'], 72) . "
----------------------------------
-
-    ";
+                $message .= wordwrap(sprintf(_("Additional message from your %s administrator:"), $cat->nomenclature_fed), 72) . "\n---------------------------------" .
+                        wordwrap($customtext[0]['value'], 72) . "\n---------------------------------\n\n";
             }
-        }
-
-        $message .= wordwrap(sprintf(_("To enlist as an administrator for that %s, please click on the following link:"), $cat->nomenclature_inst), 72) . "
-    
-$proto" . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME'])) . "/admin/action_enrollment.php?token=$newtoken
-    
-" . wordwrap(sprintf(_("If clicking the link doesn't work, you can also go to the %s Administrator Interface at"), CONFIG['APPEARANCE']['productname']), 72) . "
-    
-$proto" . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME'])) . "/admin/ 
-    
-" .
-                _("and enter the invitation token") . "
-    $newtoken
-" . ( /* $new_idp_authorized_fedadmin */ FALSE ?
-                wordwrap(sprintf(_("manually. If you reply to this mail, you will reach your %s administrators."), $cat->nomenclature_fed), 72) :
-                wordwrap(_("manually. Please do not reply to this mail; this is a send-only address.")) ) . "
-
-" . wordwrap(_("Do NOT forward the mail before the token has expired - or the recipients may be able to consume the token on your behalf!"), 72) . "
-
-" . wordwrap(sprintf(_("We wish you a lot of fun with the %s."), CONFIG['APPEARANCE']['productname']), 72) . "
-        
-" . sprintf(_("Sincerely,
-
-Your friendly folks from %s Operations"), CONFIG_CONFASSISTANT['CONSORTIUM']['display_name']);
-
-        $mail = OutsideComm::mailHandle();
-// who to whom?
-        $mail->FromName = CONFIG['APPEARANCE']['productname'] . " Invitation System";
-        if ($federation !== NULL) {
+            // and add Reply-To already now
             foreach ($federation->listFederationAdmins() as $fedadmin_id) {
                 $fedadmin = new \core\User($fedadmin_id);
                 $mailaddrAttrib = $fedadmin->getAttributes("user:email");
@@ -301,9 +267,25 @@ Your friendly folks from %s Operations"), CONFIG_CONFASSISTANT['CONSORTIUM']['di
                 $name = $nameAttrib[0]['value'] ?? sprintf(_("%s administrator"), $cat->nomenclature_fed);
                 if (count($mailaddrAttrib) > 0) {
                     $mail->addReplyTo($mailaddrAttrib[0]['value'], $name);
+                    $replyToMessage = wordwrap(sprintf(_("manually. If you reply to this mail, you will reach your %s administrators."), $cat->nomenclature_fed), 72);
                 }
             }
         }
+
+        $message .= wordwrap(sprintf(_("To enlist as an administrator for that %s, please click on the following link:"), $cat->nomenclature_inst), 72) . "\n\n" .
+                $proto . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME'])) . "/admin/action_enrollment.php?token=$newtoken\n\n" .
+                wordwrap(sprintf(_("If clicking the link doesn't work, you can also go to the %s Administrator Interface at"), CONFIG['APPEARANCE']['productname']), 72) . "\n\n" .
+                $proto . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME'])) . "/admin/\n\n" .
+                _("and enter the invitation token") . "\n\n" .
+                $newtoken . "\n\n$replyToMessage\n\n" .
+                wordwrap(_("Do NOT forward the mail before the token has expired - or the recipients may be able to consume the token on your behalf!"), 72) . "\n\n" .
+                wordwrap(sprintf(_("We wish you a lot of fun with the %s."), CONFIG['APPEARANCE']['productname']), 72) . "\n\n" .
+                sprintf(_("Sincerely,\n\nYour friendly folks from %s Operations"), CONFIG_CONFASSISTANT['CONSORTIUM']['display_name']);
+
+
+// who to whom?
+        $mail->FromName = CONFIG['APPEARANCE']['productname'] . " Invitation System";
+
         if (isset(CONFIG['APPEARANCE']['invitation-bcc-mail']) && CONFIG['APPEARANCE']['invitation-bcc-mail'] !== NULL) {
             $mail->addBCC(CONFIG['APPEARANCE']['invitation-bcc-mail']);
         }
