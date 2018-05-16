16ee5ebca5a7e3ec53b8d3d19436673b554b8f52
Stefan Winter <stefan.winter@restena.lu>
2018-05-16 09:39:34 +0200

    add openlayers copyright

--- web/copyright.php
+++ web/copyright.php
@@ -188,6 +188,14 @@ Andreas
                     <b>GeoLite and GeoLite2</b> - &copy; MaxMind, Inc.<br/>
                     This deployment of the CAT product may include GeoLite and/or GeoLite2 data created by MaxMind, available from <a href="http://www.maxmind.com">http://www.maxmind.com</a>
                 </li>
+                <li>
+                    <b>OpenLayers</b> - &copy; 2005-present OpenLayers Contributors. All rights reserved.<br/>
+                    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+                    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+                    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+                    THIS SOFTWARE IS PROVIDED BY OPENLAYERS CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+                    The views and conclusions contained in the software and documentation are those of the authors and should not be interpreted as representing official policies, either expressed or implied, of OpenLayers Contributors.
+                </li>
             </ul>
         </div>
         <div class='footer'>
