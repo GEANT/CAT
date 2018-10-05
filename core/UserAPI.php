40906be5d7f1754d21b72ac4e3518615818f7588
Stefan Winter <stefan.winter@restena.lu>
2018-10-05 14:06:20 +0200

    downloading a non-public profile did not work for the admin himself on the fine-tuning page. Looks like I missed session initialisation and authentication here. @twoln is this change something you can push to the deployment easily? This bug is seen in the wild, report by Ralf Paffrath

--- core/UserAPI.php
+++ core/UserAPI.php
@@ -88,13 +88,17 @@ class UserAPI extends CAT {
         $attribs = $profile->getCollapsedAttributes();
         if (\core\common\Entity::getAttributeValue($attribs, 'profile:production', 0) !== 'on') {
             $this->loggerInstance->debug(4, "Attempt to download a non-production ready installer for profile: $profile->identifier\n");
+            // initialise PHP session, otherwise the user ID won't end up in $_SESSION
+            CAT_session_start();
             $auth = new \web\lib\admin\Authentication();
+            $auth->authenticate();
             if (!$auth->isAuthenticated()) {
                 $this->loggerInstance->debug(2, "User NOT authenticated, rejecting request for a non-production installer\n");
                 header("HTTP/1.0 403 Not Authorized");
                 return FALSE;
             }
             $userObject = new User($_SESSION['user']);
+            print_r($_SESSION);
             if (!$userObject->isIdPOwner($profile->institution)) {
                 $this->loggerInstance->debug(2, "User not an owner of a non-production profile - access forbidden\n");
                 header("HTTP/1.0 403 Not Authorized");
