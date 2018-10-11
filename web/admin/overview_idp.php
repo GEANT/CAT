db52d0f0fd975b344ec39699b72810ff576e7f20
Stefan Winter <stefan.winter@restena.lu>
2018-10-11 09:56:02 +0200

    the admin area always displays the link to its own download area; even a global redirect leads to something being displayed there

--- web/admin/overview_idp.php
+++ web/admin/overview_idp.php
@@ -306,12 +306,8 @@ echo $mapCode->htmlHeadCode();
                 echo "<div style='width:20px;'></div>";
                 if ($readiness == core\AbstractProfile::READINESS_LEVEL_SHOWTIME) {
                     echo "<div style='display: table-cell; text-align:center;'><p><strong>" . _("User Download Link") . "</strong></p>";
-                    $URL = $profile_list->getCollapsedAttributes();
-                    if (isset($URL['device-specific:redirect'])) {
-                        $displayurl = $URL['device-specific:redirect'][0];
-                    } else {
-                        $displayurl = ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on" ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME'])) . "?idp=" . $my_inst->identifier . "&amp;profile=" . $profile_list->identifier;
-                    }
+                    $URL = $profile_list->getCollapsedAttributes();                    
+                    $displayurl = ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on" ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['SCRIPT_NAME'])) . "?idp=" . $my_inst->identifier . "&amp;profile=" . $profile_list->identifier;
                     echo "<a href='$displayurl' style='white-space: nowrap; text-align: center;'>";
                     $uri = "data:image/png;base64," . base64_encode($uiElements->pngInjectConsortiumLogo(QRcode::png($displayurl, FALSE, QR_ECLEVEL_Q, QRCODE_PIXELS_PER_SYMBOL), QRCODE_PIXELS_PER_SYMBOL));
                     $size = getimagesize($uri);
