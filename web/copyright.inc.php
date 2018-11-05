67f5773248ea122a49f774a5c1e9cb0523337d44
Stefan Winter <stefan.winter@restena.lu>
2018-11-05 11:26:12 +0100

    found another snippet of code from OpenLayers

--- web/copyright.inc.php
+++ web/copyright.inc.php
@@ -161,7 +161,7 @@ Andreas
     </li>
     <li>
         <b>OpenLayers</b> - &copy; 2005-present OpenLayers Contributors. All rights reserved.<br/>
-        (included in source distribution under web/external/OpenLayers )<br/>
+        (included in source distribution under web/external/OpenLayers and web/lib/admin/ol_drag.js)<br/>
         License: Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
         Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
         Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
