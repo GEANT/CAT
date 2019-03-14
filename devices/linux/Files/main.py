7d54ebc364a949fec665d92350d7a71b2138c140
Stefan Winter <stefan.winter@restena.lu>
2019-03-14 09:19:15 +0100

    don't complaint about raw_input; it is defined just fine on python2

--- devices/linux/Files/main.py
+++ devices/linux/Files/main.py
@@ -78,7 +78,7 @@ def byte_to_string(barray):
 
 def get_input(prompt):
     if sys.version_info.major < 3:
-        return raw_input(prompt)
+        return raw_input(prompt) # pylint: disable=undefined-variable
     return input(prompt)
 
 
