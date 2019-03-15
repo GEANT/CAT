0083d3514da30ed3208f4c29c3ec26d3165dcee8
Stefan Winter <stefan.winter@restena.lu>
2019-03-15 08:33:27 +0100

    silence false positive from scrutinizer

--- core/DeploymentManaged.php
+++ core/DeploymentManaged.php
@@ -143,7 +143,7 @@ class DeploymentManaged extends AbstractDeployment {
         }
         $propertyQuery = "SELECT status,port_instance_1,port_instance_2,secret,radius_instance_1,radius_instance_2 FROM deployment WHERE deployment_id = ?";
         $queryExec = $this->databaseHandle->exec($propertyQuery, "i", $deploymentIdRaw);
-        if (mysqli_num_rows($queryExec) == 0) {
+        if (mysqli_num_rows(/** @scrutinizer ignore-type */ $queryExec) == 0) {
             throw new Exception("Attempt to construct an unknown DeploymentManaged!");
         }
         $this->identifier = $deploymentIdRaw;
