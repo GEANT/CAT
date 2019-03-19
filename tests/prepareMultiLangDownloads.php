aa050f1d05955097e09c77d7537b31033ab135e5
Stefan Winter <stefan.winter@restena.lu>
2019-03-19 09:48:41 +0100

    special check which installs all translations, generates installers for each of them, and verifies them all. This test is very resource intensive and has to be enabled be ENVAR; typically just before a release when translations are up to date. @twoln

---
+++ tests/prepareMultiLangDownloads.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * ******************************************************************************
+ * Copyright 2011-2017 DANTE Ltd. and GÉANT on behalf of the GN3, GN3+, GN4-1 
+ * and GN4-2 consortia
+ *
+ * License: see the web/copyright.php file in the file structure
+ * ******************************************************************************
+ */
+require_once "../config/config-master-template.php";
+const OSLIST = ["chromeos", "w10", "apple_hi_sierra", "linux", "eap-config"];
+$bashLines = "";
+foreach (CONFIG['LANGUAGES'] as $lang => $details) {
+    echo "Generating bash script for generation of all language variants except English, locale = $lang";
+    foreach (OSLIST as $oneOS) {
+        $filename = "/home/scrutinizer/artifacts/$lang-$oneOS";
+        $bashLines .= "wget http://ci.test/user/API.php?action=downloadInstaller\&api_version=2\&lang=$lang\&device=$oneOS\&profile=3 -O $filename --no-verbose\n";
+        switch ($oneOS) {
+            case "chromeos":
+                $bashLines .= "cat $filename | jq -r .Type | grep UnencryptedConfiguration\n";
+                break;
+            case "w10":
+                $bashLines .= "file $filename | egrep 'executable.*Intel.*Windows.*Nullsoft'\n";
+                break;
+            case "apple_hi_sierra":
+                $bashLines .= "openssl smime -verify -in $filename -inform der -noverify 2>&1 | egrep '(Verification successful|plist)' | wc -l | grep 4\n";
+                break;
+            case "linux":
+                $bashLines .= "pylint -E $filename\n";
+                break;
+            case "eap-config":
+                $bashLines .= "xmlstarlet val -s \"/home/scrutinizer/build/devices/xml/eap-metadata.xsd\" \"$filename\"\n";
+                break;
+        }
+    }
+}
+file_put_contents("/home/scrutinizer/build/tests/langTestScript.sh", $bashLines);
---
+++ utils/installTranslations.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * ******************************************************************************
+ * Copyright 2011-2017 DANTE Ltd. and GÉANT on behalf of the GN3, GN3+, GN4-1 
+ * and GN4-2 consortia
+ *
+ * License: see the web/copyright.php file in the file structure
+ * ******************************************************************************
+ */
+require_once "../config/config-master-template.php";
+const AREAS = ["web_admin", "web_user", "devices", "core", "diagnostics"];
+foreach (CONFIG['LANGUAGES'] as $lang => $details) {
+    echo "Generating locale for ".$details['locale'];
+    exec("localegen ".$details['locale']);
+    foreach (AREAS as $oneArea) {
+        exec("msgfmt $lang/$oneArea.po -o $lang/LC_MESSAGES/$oneArea.mo");
+    }
+}
