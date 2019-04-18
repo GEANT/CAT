73048e1064cafad90bf1cad9ad6b9bdf4fe3362a
Stefan Winter <stefan.winter@restena.lu>
2019-04-18 08:10:01 +0200

    make this version 2.0.1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = CAT-2.0.0
+VERSION = CAT-2.0.1
 VV = $(VERSION)/
 .PHONY: translation
 
--- core/CAT.php
+++ core/CAT.php
@@ -55,7 +55,7 @@ class CAT extends \core\common\Entity {
      */
     const VERSION_MAJOR = 2;
     const VERSION_MINOR = 0;
-    const VERSION_PATCH = 0;
+    const VERSION_PATCH = 1;
     const VERSION_EXTRA = "";
     const RELEASE_VERSION = TRUE;
     const USER_API_VERSION = 2;
@@ -67,7 +67,7 @@ class CAT extends \core\common\Entity {
     const COPYRIGHT_HOLDER = "G&Eacute;ANT Association";
     const COPYRIGHT_CONSORTIA = "the G&Eacute;ANT Projects funded by EU";
     const COPYRIGHT_MIN_YEAR = 2011;
-    const COPYRIGHT_MAX_YEAR = 2018;
+    const COPYRIGHT_MAX_YEAR = 2019;
 
     /**
      * This is the user-displayed string; controlled by the four options above
