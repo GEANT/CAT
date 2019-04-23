3a4dc79fc21845d5b30085eba724738c8a33487b
Stefan Winter <stefan.winter@restena.lu>
2019-04-23 15:19:58 +0200

    it is only complaining about an instance where something else has the same prefix as variable name ($var) so maybe the residual complaints have to do with naming?

--- core/common/Entity.php
+++ core/common/Entity.php
@@ -130,14 +130,14 @@ abstract class Entity {
         if (strlen($dummy_NRO . $dummy_inst1 . $dummy_inst2 . $dummy_inst3 . $dummy_hotspot1 . $dummy_hotspot2 . $dummy_hotspot3 . $dummy_organisation1 . $dummy_organisation2 . $dummy_organisation2a . $dummy_organisation3) < 0) {
             throw new Exception("Strings are usually not shorter than 0 characters. We've encountered a string blackhole.");
         }
-        $variableFed = \config\ConfAssistant::CONSORTIUM['nomenclature_federation'] . "";
-        $variableInst = \config\ConfAssistant::CONSORTIUM['nomenclature_institution'] . "";
-        $variableHotspot = \config\ConfAssistant::CONSORTIUM['nomenclature_hotspot'] . "";
-        $variableParticipant = \config\ConfAssistant::CONSORTIUM['nomenclature_participant'] . "";
-        Entity::$nomenclature_fed = _($variableFed);
-        Entity::$nomenclature_inst = _($variableInst);
-        Entity::$nomenclature_hotspot = _($variableHotspot);
-        Entity::$nomenclature_participant = _($variableParticipant);
+        $xyzVariableFed = \config\ConfAssistant::CONSORTIUM['nomenclature_federation'] . "";
+        $xyzVariableInst = \config\ConfAssistant::CONSORTIUM['nomenclature_institution'] . "";
+        $xyzVariableHotspot = \config\ConfAssistant::CONSORTIUM['nomenclature_hotspot'] . "";
+        $xyzVariableParticipant = \config\ConfAssistant::CONSORTIUM['nomenclature_participant'] . "";
+        Entity::$nomenclature_fed = _($xyzVariableFed);
+        Entity::$nomenclature_inst = _($xyzVariableInst);
+        Entity::$nomenclature_hotspot = _($xyzVariableHotspot);
+        Entity::$nomenclature_participant = _($xyzVariableParticipant);
 
         Entity::outOfThePotatoes();
     }
