553fd0001d9fd8735822e5ee949429c8cc4fa00a
Stefan Winter <stefan.winter@restena.lu>
2019-04-24 12:23:02 +0200

    good point; on branch we only need a total number. makes query faster. @twoln

--- core/ExternalEduroamDBData.php
+++ core/ExternalEduroamDBData.php
@@ -47,7 +47,7 @@ use \Exception;
  */
 class ExternalEduroamDBData extends EntityWithDBProperties {
 
-    private $SPList = [];
+    private $counter = -1;
 
     /**
      * constructor, gives us access to the DB handle we need for queries
@@ -100,13 +100,13 @@ class ExternalEduroamDBData extends EntityWithDBProperties {
      * @return array list of providers
      */
     public function allServiceProviders() {
-        if (count($this->SPList) == 0) {
-            $query = $this->databaseHandle->exec("SELECT country, inst_name, sp_location FROM view_active_SP_location_eduroamdb");
+        if ($this->counter == -1) {
+            $query = $this->databaseHandle->exec("SELECT count(*) AS total FROM view_active_SP_location_eduroamdb");
             while ($iterator = mysqli_fetch_object(/** @scrutinizer ignore-type */ $query)) {
-                $this->SPList[] = ["country" => $iterator->country, "instnames" => $this->splitNames($iterator->inst_name), "locnames" => $this->splitNames($iterator->sp_location)];
+                $this->counter = $iterator->total;
             }
         }
-        return $this->SPList;
+        return $this->counter;
     }
 
 }
