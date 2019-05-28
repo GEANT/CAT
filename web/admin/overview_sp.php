3d7ab7728834c69175f12c5d68c5ff1027bb78d3
Maja Gorecka-Wolniewicz <mgw@umk.pl>
2019-05-28 15:45:18 +0200

    fixes

--- web/admin/overview_sp.php
+++ web/admin/overview_sp.php
@@ -84,8 +84,7 @@ echo $mapCode->htmlHeadCode();
         curl_setopt ( $ch, CURLOPT_URL, 'http://'.$host );
         curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
         curl_setopt ( $ch, CURLOPT_TIMEOUT, $timeout );
-        $http_respond = curl_exec($ch);
-        $http_respond = trim( strip_tags( $http_respond ) );
+        curl_exec($ch);
         $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
         if ($http_code == 200) {
             return 1;
