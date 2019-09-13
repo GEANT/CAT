1fd1785d8b258640139dc52726b7151497fdc33f
Stefan Winter <stefan.winter@restena.lu>
2019-09-13 14:57:52 +0200

    this is a released version now

--- core/CAT.php
+++ core/CAT.php
@@ -57,7 +57,7 @@ class CAT extends \core\common\Entity {
     const VERSION_MINOR = 0;
     const VERSION_PATCH = 2;
     const VERSION_EXTRA = "";
-    const RELEASE_VERSION = FALSE;
+    const RELEASE_VERSION = TRUE;
     const USER_API_VERSION = 2;
 
     /**
