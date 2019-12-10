e9c599382e1b28a25dfdfd42eb19b04c064b372e
Stefan Winter <stefan.winter@restena.lu>
2019-12-10 08:41:20 +0100

    SSP is now using namespaces for this class

--- web/lib/admin/Authentication.php
+++ web/lib/admin/Authentication.php
@@ -40,7 +40,7 @@ class Authentication extends \core\common\Entity {
      */
     public function isAuthenticated() {
         $authSimple = new \SimpleSAML\Auth\Simple(CONFIG['AUTHENTICATION']['ssp-authsource']);
-        $session = \SimpleSAML_Session::getSessionFromRequest();
+        $session = \SimpleSAML\Session::getSessionFromRequest();
         $status = $authSimple->isAuthenticated();
         $session->cleanup();
         return $status;
@@ -58,7 +58,7 @@ class Authentication extends \core\common\Entity {
         $authSimple = new \SimpleSAML\Auth\Simple(CONFIG['AUTHENTICATION']['ssp-authsource']);
         $authSimple->requireAuth();
         $admininfo = $authSimple->getAttributes();
-        $session = \SimpleSAML_Session::getSessionFromRequest();
+        $session = \SimpleSAML\Session::getSessionFromRequest();
         $session->cleanup();
 
         if (!isset($admininfo[CONFIG['AUTHENTICATION']['ssp-attrib-identifier']][0])) {
