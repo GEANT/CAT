46820e3a34b91e0404833b1ff5bd04cd61be63f6
Stefan Winter <stefan.winter@restena.lu>
2021-03-03 09:19:43 +0100

    up the version for a future release

--- core/CAT.php
+++ core/CAT.php
@@ -55,7 +55,7 @@ class CAT extends \core\common\Entity {
      */
     const VERSION_MAJOR = 2;
     const VERSION_MINOR = 0;
-    const VERSION_PATCH = 4;
+    const VERSION_PATCH = 5;
     const VERSION_EXTRA = "";
     const RELEASE_VERSION = FALSE;
     const USER_API_VERSION = 2;
