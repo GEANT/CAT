b6d52fdb0b29b316aa753d6312d6c4c49fabd844
Stefan Winter <stefan.winter@restena.lu>
2021-08-06 09:33:34 +0200

    fix notices

--- devices/chromebook/DeviceChromebook.php
+++ devices/chromebook/DeviceChromebook.php
@@ -99,8 +99,8 @@ class DeviceChromebook extends \core\DeviceConfig
     {
         parent::__construct();
         $this->setSupportedEapMethods([\core\common\EAP::EAPTYPE_PEAP_MSCHAP2, \core\common\EAP::EAPTYPE_TTLS_PAP, \core\common\EAP::EAPTYPE_TTLS_MSCHAP2, \core\common\EAP::EAPTYPE_TLS, \core\common\EAP::EAPTYPE_SILVERBULLET]);
-        $this->specialities['media:openroaming'][serialize(\core\common\EAP::EAPTYPE_PEAP_MSCHAP2)] = _("This device does not support provisioning of OpenRoaming.");
-        $this->specialities['media:consortium_OI'][serialize(\core\common\EAP::EAPTYPE_PEAP_MSCHAP2)] = _("This device does not support provisioning of Passpoint networks.");
+        $this->specialities['media:openroaming'] = _("This device does not support provisioning of OpenRoaming.");
+        $this->specialities['media:consortium_OI'] = _("This device does not support provisioning of Passpoint networks.");
     }
 
     /**
