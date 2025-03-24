80d67d6c9e570c70ff2beee098b9e678ec825e6d
Maja Górecka-Wolniewicz <mgw@cat-ams-test.eduroam.org>
2025-03-24 13:37:18 +0100

    sendzip.inc.php renamed

--- web/admin/overview_sp.php
+++ web/admin/overview_sp.php
@@ -1,5 +1,33 @@
 <?php
 
+/*
+ * *****************************************************************************
+ * Contributions to this work were made on behalf of the GÉANT project, a 
+ * project that has received funding from the European Union’s Framework 
+ * Programme 7 under Grant Agreements No. 238875 (GN3) and No. 605243 (GN3plus),
+ * Horizon 2020 research and innovation programme under Grant Agreements No. 
+ * 691567 (GN4-1) and No. 731122 (GN4-2).
+ * On behalf of the aforementioned projects, GEANT Association is the sole owner
+ * of the copyright in all material which was developed by a member of the GÉANT
+ * project. GÉANT Vereniging (Association) is registered with the Chamber of 
+ * Commerce in Amsterdam with registration number 40535155 and operates in the 
+ * UK as a branch of GÉANT Vereniging.
+ * 
+ * Registered office: Hoekenrode 3, 1102BR Amsterdam, The Netherlands. 
+ * UK branch address: City House, 126-130 Hills Road, Cambridge CB2 1PQ, UK
+ *
+ * License: see the web/copyright.inc.php file in the file structure or
+ *          <base_url>/copyright.php after deploying the software
+ * */
+ 
+ /**
+ * This file is used to pack and send RADSEC credentials.
+ * 
+ * @author Maja Gorecka-Wolniewicz <mgw@umk.pl>
+ */
+?>
+<?php
+
 $error_message = [
     'WRONGCSR' => _('The uploaded file does not contain a valid CSR!'),
     'NOCSR' => _('The provided file can not be uploaded.'),
@@ -216,7 +244,7 @@ function displayDeploymentPropertyWidget(&$deploymentObject, $errormsg=[]) {
                             <?php } ?>
                             <button class="sp_cert" id="cert_<?php echo $deploymentObject->identifier;?>" name="showp" type="submit"><?php echo _('certificate');?></button>
                             <button class="ca_cert" name="showca" type="submit"><?php echo _('CA certificate');?></button>
-                            <button name="sendzip" onclick="location.href='inc/sendzip.inc.php?inst_id=<?php echo $deploymentObject->institution;?>&dep_id=<?php echo $deploymentObject->identifier;?>'" type="button"><?php echo _('download ZIP-file with full data');?></button>
+                            <button name="sendzip" onclick="location.href='inc/sendZip.inc.php?inst_id=<?php echo $deploymentObject->institution;?>&dep_id=<?php echo $deploymentObject->identifier;?>'" type="button"><?php echo _('download ZIP-file with full data');?></button>
                         </td>
                 </tr>
                 <tr> <td></td><td>
