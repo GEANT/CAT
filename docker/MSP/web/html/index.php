be5cf05539bc91c5b9efc7a6e19cc4c459e2843c
Maja Górecka-Wolniewicz <mgw@cat-ams-test.eduroam.org>
2025-07-23 20:58:41 +0200

    MSP - encrypted data from/to CAT

--- docker/MSP/web/html/index.php
+++ docker/MSP/web/html/index.php
@@ -4,59 +4,85 @@ define("ZIPDIR", '/opt/FR/var/log/forCAT/');
 $remove = 0;
 $opn = $vlans = '';
 $guest_vlan = 0;
-error_log(serialize($_REQUEST));
+error_log(serialize($_POST));
+error_log(SERVER_SECRET);
+if ( isset($_POST['enc']) ) {
+	error_log("enc: ".$_POST['enc']);
+	$decrypted = openssl_decrypt(base64_decode($_POST['enc']), "CHACHA20", SERVER_SECRET, 0, SERVER_IV);
+	if ($decrypted === false) {
+          echo "FAILURE";
+          exit;	  
+	} else {
+          error_log("data=$decrypted");
+	  parse_str($decrypted, $darr);
+	  error_log(serialize($darr));
+	  if (!isset($darr['token']) || $darr['token'] != SERVER_TOKEN) {
+		  echo "FAILURE";
+		  exit;
+          } 
+	  error_log("GREAT!");
+	} 
+}
 # when a request contains logid and backlog ";s:11:"DEBUG-11-52";s:7:"backlog";s:1:"7";}
-if ( isset($_REQUEST['logid']) && isset($_REQUEST['backlog']) ) {
-	if (substr($_REQUEST['logid'], 0, 5) == 'DEBUG') {
-          $logid = substr($_REQUEST['logid'], 6);
+if ( isset($darr['logid']) && isset($darr['backlog']) && isset($darr['iv']) ) {
+	if (substr($darr['logid'], 0, 5) == 'DEBUG') {
+          $logid = substr($darr['logid'], 6);
         }
-  	$res = cat_socket(implode(':', array($logid, $_REQUEST['backlog'])));
+	$iv = $darr['iv'];
+  	$res = cat_socket(implode(':', array($logid, $darr['backlog'])));
+	$cnt = 0;
 	error_log('GOT '.$res);
 	if (substr($res, 0, strlen(ZIPDIR)) == ZIPDIR) {
             error_log('GOT filename '.$res);
             $za = new ZipArchive();
             $za->open($res);
-	    header('Content-Type: application/zip');
-            header("Content-Disposition: attachment; filename=\"detail_".$logid.".zip\"");
+	    $cnt = $za->numFiles;
+	    $content = file_get_contents($res);
+            $encrypted = openssl_encrypt(SERVER_TOKEN . $content, "CHACHA20", SERVER_SECRET, 0, $iv);
+	    header('Content-Type: application/octet-stream');
+            header("Content-Disposition: attachment; filename=\"detail_".$logid.".enc\"");
             header("Content-Transfer-Encoding: binary");
-	    echo 'ZIPDATA:'.file_get_contents($res);
+	    echo "ZIPDATA:$encrypted";
 	    error_log('Sent data in response');
 	}
-        error_log('with '.$za->numFiles . ' files');
+        error_log('with '. $cnt . ' files');
+	if (file_exists($res)) {
+	  unlink($res);
+	}
 	exit;
 }
 # MUST provide: deployment_id, inst_id
 #               and port, secret, pskkey, country or torevoke
 # MAY provide: operatorname, vlanno, vlanrealms[], guest_vlan, remove
 if (
-    isset($_REQUEST['instid']) && isset($_REQUEST['deploymentid']) &&
-    (isset($_REQUEST['port']) && isset($_REQUEST['secret']) && isset($_REQUEST['pskkey']) && isset($_REQUEST['country']) ||
-     isset($_REQUEST['torevoke']))) {
-  if (isset($_REQUEST['remove'])) {
+    isset($darr['instid']) && isset($darr['deploymentid']) &&
+    (isset($darr['port']) && isset($darr['secret']) && isset($darr['pskkey']) && isset($darr['country']) ||
+     isset($darr['torevoke']))) {
+  if (isset($darr['remove'])) {
     $remove = 1;
   } else {
-    if (isset($_REQUEST['operatorname'])) {
-      $opn = trim($_REQUEST['operatorname']);
+    if (isset($darr['operatorname'])) {
+      $opn = trim($darr['operatorname']);
     }
-    if (isset($_REQUEST['vlan']) && isset($_REQUEST['realmforvlan']) &&
-        is_array($_REQUEST['realmforvlan'])) {
-      $vlans = $_REQUEST['vlan'] . '#' . implode('#', $_REQUEST['realmforvlan']);
+    if (isset($darr['vlan']) && isset($darr['realmforvlan']) &&
+        is_array($darr['realmforvlan'])) {
+      $vlans = $darr['vlan'] . '#' . implode('#', $darr['realmforvlan']);
     }
-    if (isset($_REQUEST['guest_vlan'])) {
-      $guest_vlan = $_REQUEST['guest_vlan'];
+    if (isset($darr['guest_vlan'])) {
+      $guest_vlan = $darr['guest_vlan'];
     }
   }
-  if (isset($_REQUEST['torevoke'])) {
-	  $el = explode('#', $_REQUEST['torevoke']);
-	  $res = cat_socket(implode(':', array($_REQUEST['instid'], $_REQUEST['deploymentid'], $el[0], $el[1])));
+  if (isset($darr['torevoke'])) {
+	  $el = explode('#', $darr['torevoke']);
+	  $res = cat_socket(implode(':', array($darr['instid'], $darr['deploymentid'], $el[0], $el[1])));
   } else {
   	# arguments 5-7 are Base64 encoded
-  	$res = cat_socket(implode(':', array($_REQUEST['country'],
-                                 $_REQUEST['instid'], $_REQUEST['deploymentid'],
-                                 $_REQUEST['port'],
-                                 base64_encode($_REQUEST['secret']),
+  	$res = cat_socket(implode(':', array($darr['country'],
+                                 $darr['instid'], $darr['deploymentid'],
+                                 $darr['port'],
+                                 base64_encode($darr['secret']),
                                  base64_encode($opn), 
-                                 base64_encode($vlans), base64_encode($_REQUEST['pskkey']), $guest_vlan, $remove)));
+                                 base64_encode($vlans), base64_encode($darr['pskkey']), $guest_vlan, $remove)));
   }
   echo $res;
 } else {
